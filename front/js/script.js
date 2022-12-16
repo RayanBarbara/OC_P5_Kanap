8908d95801c20c07df9e7edb48e17e6b6755e345
RayanBarbara <barbara.rayan.1998@gmail.com>
2022-12-16 20:44:25 +0100

    Fetch and display all products on homepage

---
+++ front/js/script.js
@@ -0,0 +1,25 @@
+fetch('http://localhost:3000/api/products')
+.then((response) => response.json())
+.then((data) => {
+    console.table(data);
+    listAllProducts(data);
+})
+.catch((error) => {
+    console.error('Error:', error);
+});
+
+function listAllProducts(productsList){
+    let productsDisplayZone = document.getElementById('items');
+    for(product of productsList){
+        let productInformation = document.createElement('a');
+        productInformation.innerHTML = 
+        `<a href="./product.html?id=${product._id}">
+        <article>
+          <img src="${product.imageUrl}" alt="${product.altTxt}">
+          <h3 class="productName">${product.name}</h3>
+          <p class="productDescription">${product.description}</p>
+        </article>
+      </a>`;
+      productsDisplayZone.appendChild(productInformation);
+    }
+}
