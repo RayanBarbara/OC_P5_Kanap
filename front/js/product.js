f7a4ab059cbb0b037fca7ea5e892b59205bae036
RayanBarbara <barbara.rayan.1998@gmail.com>
2022-12-24 16:10:09 +0100

    Added products to cart + avoided duplicates

--- front/js/product.js
+++ front/js/product.js
@@ -1,43 +1,36 @@
+let productID = new URLSearchParams(window.location.search).get('id');
+
 function getProductInformation() {
-    let productID = new URLSearchParams(window.location.search).get('id');
-    fetch('http://localhost:3000/api/products/' +productID)
+    fetch('http://localhost:3000/api/products/' + productID)
         .then((response) => response.json())
         .then((data) => {
-            //console.table(data);
             let productImgURL = data.imageUrl;
-            //console.log(productImgURL);
             let productAltText = data.altTxt;
-            //console.log(productAltText);
             let productName = data.name;
-            //console.log(productName);
             let productPrice = data.price;
-            //console.log(productPrice);
             let productDescription = data.description;
-            //console.log(productDescription);
             let productColor = data.colors;
-            //console.log(productColor);
-            displayProducts(productImgURL, productAltText, productName, 
-                productPrice, productDescription, productColor);
+            displayProducts(productImgURL, productAltText, productName, productPrice, productDescription, productColor);
         })
         .catch((error) => {
             console.error('Error:', error);
         });
 }
 
-function displayProducts(imgURL, altTxt, name, price, description, colors){
+function displayProducts(imgURL, altTxt, name, price, description, colors) {
     let productImgURL = document.querySelector('.item__img');
     let productName = document.getElementById('title');
     let productPrice = document.getElementById('price');
     let productDescription = document.getElementById('description');
     let productColor = document.getElementById('colors');
-    productImgURL.innerHTML =`
+    productImgURL.innerHTML = `
     <div id="item__img">
         <img id='item__image' src="${imgURL}" alt="${altTxt}">
     </div>`;
     productName.textContent = name;
     productPrice.textContent = price;
     productDescription.textContent = description;
-    for(color in colors){
+    for (color in colors) {
         let colorOption = document.createElement('option');
         colorOption.innerHTML = `
         <option value="${colors[color]}">${colors[color]}</option>`;
@@ -45,4 +38,40 @@ function displayProducts(imgURL, altTxt, name, price, description, colors){
     }
 }
 
+document.getElementById('addToCart').addEventListener('click', () => {
+    let bool = false;
+    let command = {
+        id: productID,
+        color: document.getElementById('colors').value,
+        quantity: +document.getElementById('quantity').value
+    };
+    if (command.quantity === 0 && command.color === '') {
+        alert('Please select the color and quantity!');
+    } else if (command.color === '') {
+        alert('Please select the color!');
+    } else if (command.quantity === 0) {
+        alert('Please select the quantity!');
+    }
+    else {
+        for (let i = 0; i < localStorage.length; i++) {
+            if (command.id.normalize() === JSON.parse(localStorage.getItem(i)).id.normalize() && command.color.normalize() === JSON.parse(localStorage.getItem(i)).color.normalize()) {
+                command.quantity += +JSON.parse(localStorage.getItem(i)).quantity;
+                localStorage.setItem(i, JSON.stringify(command));
+                bool = false;
+                break;
+            } else {
+                bool = true;
+            }
+        } if (bool) {
+            localStorage.setItem(localStorage.length, JSON.stringify(command));
+        }
+    }
+});
+
+let command = {
+    id: '0',
+    color: 'Test',
+    quantity: +"0"
+};
+localStorage.setItem(0, JSON.stringify(command));
 getProductInformation();
--- front/js/script.js
+++ front/js/script.js
@@ -1,7 +1,6 @@
 fetch('http://localhost:3000/api/products')
 .then((response) => response.json())
 .then((data) => {
-    //console.table(data);
     listAllProducts(data);
 })
 .catch((error) => {
